e6aea94f7ed306f4052d003c4f95f2560de69145
Felix Meyer-Veit <felix.meyer-veit@web.de>
2020-11-23 16:16:46 +0000

    Mark brightest spot in the image

--- Assignment 1 - OpenCV setup and point operations/01_assignment.py
+++ Assignment 1 - OpenCV setup and point operations/01_assignment.py
@@ -42,8 +42,20 @@ while (True):
     # puting the FPS count on the frame
     cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
+    # convert image to grayscale
+    orig = frame.copy()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # apply a Gaussian blur to the image then find the brightest
+    # region
+    gray = cv2.GaussianBlur(gray, (1, 1), 0)
+
+    # perform a naive attempt to find the (x, y) coordinates of
+    # the area of the image with the largest intensity value
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+    cv2.circle(frame, maxLoc, 5, (255, 0, 0), 2)
+
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('Webcam', frame)
 
     # the 'q' button is set as the
     # quitting button you may use any
