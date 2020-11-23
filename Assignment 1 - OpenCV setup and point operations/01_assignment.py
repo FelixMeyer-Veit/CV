bc6049e281d4ae548e8c821e3291c134b7dba11b
Felix Meyer-Veit <felix.meyer-veit@web.de>
2020-11-23 23:49:07 +0000

    finished

--- Assignment 1 - OpenCV setup and point operations/01_assignment.py
+++ Assignment 1 - OpenCV setup and point operations/01_assignment.py
@@ -1,8 +1,10 @@
+################################################ IMPORT ################################################################
 # import the opencv library
 import cv2
 import time
-import numpy as np
+import numpy
 
+################################################ DECLARATIONS ##########################################################
 # define a video capture object
 vid = cv2.VideoCapture(0)
 
@@ -12,20 +14,152 @@ prev_frame_time = 0
 # used to record the time at which we processed current frame
 new_frame_time = 0
 
-fps_1 = fps_2 = fps_3 = iteration = 0
+# create array for getting average
+lst = []
 
-while (True):
+################################################ FUNCTIONS #############################################################
+# get average of a list
+def Average(lst):
+    return sum(lst) / len(lst)
 
-    iteration += 1
+def getSpot(dest, frame,g, b, color):
+    # convert image to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    g_gray = cv2.cvtColor(g, cv2.COLOR_BGR2GRAY)
+    b_gray = cv2.cvtColor(b, cv2.COLOR_BGR2GRAY)
+
+    # apply a Gaussian blur to the image then find the brightest region
+    gray = cv2.GaussianBlur(gray, (1, 1), 0)
+    g_gray = cv2.GaussianBlur(g_gray, (1, 1), 0)
+    b_gray = cv2.GaussianBlur(b_gray, (1, 1), 0)
+
+    # get reddest spot
+    if color == 1:
+        gray = gray - g_gray - b_gray
+
+    # perform a naive attempt to find the (x, y) coordinates of
+    # the area of the image with the largest intensity value
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+
+    # puting the Location on the frame
+    if color == 1:
+        cv2.circle(dest, maxLoc, 5, (0, 0, 255), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (400, 450), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+    elif color == 0:
+        cv2.circle(dest, maxLoc, 5, (255, 0, 0), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (7, 450), font, 1, (255, 0, 0), 1, cv2.LINE_AA)
+
+'''
+def getSpot(dest, frame, color):
+    # convert image to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # apply a Gaussian blur to the image then find the brightest region
+    gray = cv2.GaussianBlur(gray, (1, 1), 0)
+
+    # perform a naive attempt to find the (x, y) coordinates of
+    # the area of the image with the largest intensity value
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+
+    # puting the Location on the frame
+    if color == 1:
+        cv2.circle(dest, maxLoc, 5, (0, 0, 255), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (400, 450), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+    elif color == 0:
+        cv2.circle(dest, maxLoc, 5, (255, 0, 0), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (7, 450), font, 1, (255, 0, 0), 1, cv2.LINE_AA)
+'''
+
+def getSpot2(dest, frame, g, b, color):
+    # convert image to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    g_gray = cv2.cvtColor(g, cv2.COLOR_BGR2GRAY)
+    b_gray = cv2.cvtColor(b, cv2.COLOR_BGR2GRAY)
+
+    # apply a Gaussian blur to the image then find the brightest region
+    gray = cv2.GaussianBlur(gray, (1, 1), 0)
+    g_gray = cv2.GaussianBlur(g_gray, (1, 1), 0)
+    b_gray = cv2.GaussianBlur(b_gray, (1, 1), 0)
+
+    # get reddest spot
+    if color == 1:
+        gray = gray - g_gray - b_gray
+
+    # perform a naive attempt to find the (x, y) coordinates of
+    # the area of the image with the largest intensity value
+
+    rows, cols = gray.shape[:2]
+    max = 0
+    maxLoc = (0,0)
+    for x in range(cols):
+        for y in range(rows):
+            temp = frame[y, x]
+            if temp[2] > max:
+                max = temp[2]
+                maxLoc = (y, x)
+
+    if color == 0:
+        cv2.circle(dest, maxLoc, 5, (255, 0, 0), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (7, 450), font, 1, (255, 0, 0), 1, cv2.LINE_AA)
+    elif color == 1:
+        cv2.circle(dest, maxLoc, 5, (0, 0, 255), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (400, 450), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
 
 
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
+'''
+# get reddest spot with exact subtraction/calculation --> takes more time
 
-    # font which we will be using to display FPS
-    font = cv2.FONT_HERSHEY_SIMPLEX
+def getSpot2(dest, frame, color):
+    # convert image to grayscale
+    if color == 0:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # apply a Gaussian blur to the image then find the brightest region
+        gray = cv2.GaussianBlur(gray, (1, 1), 0)
+
+        # perform a naive attempt to find the (x, y) coordinates of
+        # the area of the image with the largest intensity value
+
+        rows, cols = gray.shape[:2]
+        max = 0
+        maxLoc = (0,0)
+        for x in range(cols):
+            for y in range(rows):
+                temp = frame[y, x]
+                if temp[2] > max:
+                    max = temp[2]
+                    maxLoc = (y, x)
+
+        cv2.circle(dest, maxLoc, 5, (255, 0, 0), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (7, 450), font, 1, (255, 0, 0), 1, cv2.LINE_AA)
+    elif color == 1:
+        # perform a naive attempt to find the (x, y) coordinates of
+        # the area of the image with the largest intensity value
+
+        rows, cols = dest.shape[:2]
+        max = 0
+        maxLoc = (0, 0)
+        for x in range(cols):
+            for y in range(rows):
+                temp = dest[y, x]
+                sum = int(temp[2]) - int(temp[1]) - int(temp[0])
+                if sum > max:
+                    max = sum
+                    maxLoc = (y, x)
+
+        cv2.circle(dest, maxLoc, 5, (0, 0, 255), 2)
+        maxLoc = str(maxLoc)
+        cv2.putText(dest, maxLoc, (400, 450), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+'''
+
+def get_fps(frame, prev_frame_time):
     # time when we finish processing for this frame
     new_frame_time = time.time()
 
@@ -40,41 +174,52 @@ while (True):
     # converting the fps into integer
     fps = int(fps)
 
-    if iteration == 1:
-        fps_1 = fps
-    elif iteration == 2:
-        fps_2 = fps
-    elif iteration == 3:
-        fps_3 = fps
-        iteration = 0
+    # add current fps at the end of array
+    lst.append(fps)
 
-    fps_avg = (fps_1 + fps_2 + fps_3) / 3
-
-    fps_avg = int(fps_avg)
+    # get the average of processing time
+    average = int(Average(lst))
 
     # converting the fps to string so that we can display it on frame
     # by using putText function
-    fps_avg = str(fps_avg)
+    fps_avg = str(average)
 
     # puting the FPS count on the frame
     cv2.putText(frame, fps_avg, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
-    
 
-    # convert image to grayscale
-    orig = frame.copy()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # apply a Gaussian blur to the image then find the brightest
-    # region
-    gray = cv2.GaussianBlur(gray, (1, 1), 0)
+    return prev_frame_time
 
-    # perform a naive attempt to find the (x, y) coordinates of
-    # the area of the image with the largest intensity value
-    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-    cv2.circle(frame, maxLoc, 5, (255, 0, 0), 2)
+################################################ MAIN ##################################################################
+while (True):
 
-    maxLoc = str(maxLoc)
-    # puting the Location on the frame
-    cv2.putText(frame, maxLoc, (7, 450), font, 1, (255, 0, 0), 1, cv2.LINE_AA)
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+
+    # font which we will be using to display FPS
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    # get fps and store a new prev_frame_time
+    prev_frame_time = get_fps(frame, prev_frame_time)
+
+    # get the red channel
+    r = frame.copy()
+    # set blue and green channels to 0
+    r[:, :, 0] = 0
+    r[:, :, 1] = 0
+
+    b = frame.copy()
+    # set green and red channels to 0
+    b[:, :, 1] = 0
+    b[:, :, 2] = 0
+
+    g = frame.copy()
+    # set blue and red channels to 0
+    g[:, :, 0] = 0
+    g[:, :, 2] = 0
+
+    getSpot2(frame, r, g, b, 1) # 1 = get reddest spot
+    getSpot2(frame, frame, g, b, 0)  # 0 = get brightest spot
 
     # Display the resulting frame
     cv2.imshow('Webcam', frame)
@@ -84,6 +229,13 @@ while (True):
     # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
+# Driver Code
+average = Average(lst)
+
+# Printing average of the list
+print("Average processing time =", round(average, 2))
 
 # After the loop release the cap object
 vid.release()
